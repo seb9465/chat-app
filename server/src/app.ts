582ca059c84bd178914d1772041eb667ea4bea4c
Sebastien Cadorette <cado.94@me.com>
2018-05-02 09:52:52 -0400

    Added modules on server side + middleware configs in a function.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,16 +2,33 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
+import * as logger from 'morgan';
+import * as cors from 'cors';
 
 export class App {
     private app: express.Application;
 
     public constructor() {
         this.app = express();
+        this.middlewaresConfigs();
+        this.mountRoutes();
+    }
+
+    private middlewaresConfigs(): void {
+        this.app.use(logger('dev'));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(cookieParser());
-        this.mountRoutes();
+        this.app.use(express.static(path.join(__dirname, '../client')));
+        this.app.use(cors());
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            res.header("Access-Control-Allow-Credentials", "true");
+            res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+            res.header("Access-Control-Allow-Headers",
+                "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization");
+            next();
+        });
     }
 
     private mountRoutes(): void {
