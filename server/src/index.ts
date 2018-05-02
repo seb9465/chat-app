68feab23f2c4c326af7d47e1fc6faabfafbf9a00
Sebastien Cadorette <cado.94@me.com>
2018-05-02 11:15:16 -0400

    Added container from inversify on the server side.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { App } from './app';
 import 'reflect-metadata';
-
+import { container } from './inversify.config';
 
 const app: App = new App();
 
--- server/src/inversify.config.ts
+++ server/src/inversify.config.ts
@@ -1,1 +1,8 @@
 import { Container } from 'inversify';
+import { App } from './app';
+
+const container: Container = new Container();
+
+container.bind("App").to(App);
+
+export { container };
