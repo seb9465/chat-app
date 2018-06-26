73fa641a8465437d5e8d3322de6255b05e0440e6
Sébastien Cadorette <cado.94@me.com>
2018-06-26 14:20:33 -0400

    Sidenav + links to the 2 created components.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'app';
+export class AppComponent implements OnInit {
+    @ViewChild('sidenav')
+    sidenav: MatSidenav ;
+
+    public _callbackFunction: Function;
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        this._callbackFunction = this.toggleSideMenu.bind(this);
+    }
+
+    private toggleSideMenu() {
+        this.sidenav.toggle();
+    }
 }
