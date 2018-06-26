fd626c764ac8cf150ee62e8a55767485e023692b
Sébastien Cadorette <cado.94@me.com>
2018-06-26 14:55:18 -0400

    Calling scss file instead of css files.

--- client/src/app/common/sidenav-menu/sidenav-menu.component.ts
+++ client/src/app/common/sidenav-menu/sidenav-menu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidenav-menu',
   templateUrl: './sidenav-menu.component.html',
-  styleUrls: ['./sidenav-menu.component.css']
+  styleUrls: ['./sidenav-menu.component.scss']
 })
 export class SidenavMenuComponent implements OnInit {
 
