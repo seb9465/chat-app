9ef4feb51cb9ef7e91a04adb2865ab316320121e
Sébastien Cadorette <cado.94@me.com>
2018-06-26 14:56:12 -0400

    Rearranged imports.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-
-import { AppComponent } from './app.component';
 import { TopMenuComponent } from './common/top-menu/top-menu.component';
 import { SidenavMenuComponent } from './common/sidenav-menu/sidenav-menu.component';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
