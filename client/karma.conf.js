92b9228c7a016d53be7db83965060f4c1490be52
Sébastien Cadorette <cado.94@me.com>
2018-06-27 10:21:10 -0400

    codecov configs

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      'karma-coverage'
+      require('karma-coverage')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
