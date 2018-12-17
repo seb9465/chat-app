e611258389da79c4f0aa8541bfa6ea2a9ae4df78
SebastienCadorette <cado.94@me.com>
2018-12-17 11:00:35 -0500

    new routes for the authenticaiton

--- client/src/app/app-routing/app-routing.module.ts
+++ client/src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuUnComponent } from '../menu-un/menu-un.component';
+import { AuthGuard } from '../authentication/_guards/auth.gards';
+import { LoginComponent } from '../authentication/login/login.component';
+import { HomeComponent } from '../authentication/home/home.component';
 import { MenuDeuxComponent } from '../menu-deux/menu-deux.component';
 
 const routes: Routes = [
-    { path: 'menu1', component: MenuUnComponent },
-    { path: 'menu2', component: MenuDeuxComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'menu1', component: MenuUnComponent, canActivate: [AuthGuard] },
+    { path: 'menu2', component: MenuDeuxComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
