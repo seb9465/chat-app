e1fa8b838f638302a6718f3651a6d0ca6c4dadf2
SebastienCadorette <cado.94@me.com>
2019-03-06 21:05:24 -0500

    Ajustement de la classe MongoDB avec le nouveau package mongodb.

--- server/src/BD/MongoDB.ts
+++ server/src/BD/MongoDB.ts
@@ -1,28 +1,33 @@
-import { Mongoose } from 'mongoose';
+import { MongoClient, Collection } from 'mongodb';
 import { injectable } from 'inversify';
 
-const DB_URL: string = 'mongodb+srv://admin:admin@chat-test-zpcun.mongodb.net/test?retryWrites=true';
+const URI: string = 'mongodb+srv://admin:admin@chat-test-zpcun.mongodb.net/test?retryWrites=true';
 
 @injectable()
 export class MongoDB {
-    private _mongoose: Mongoose;
-    // private _schema: Schema;
-    // private _model: Model<Document>;
+    private _mongoClient: MongoClient;
+    private _collection: Collection;
 
     constructor() {
-        this._mongoose = new Mongoose();
+        this._mongoClient = new MongoClient(URI, { useNewUrlParser: true });
         // this._schema = new Schema(sch);
         // this._model = this._mongoose.model(modelName, this._schema);
         this.connectToBD();
     }
 
     private async connectToBD(): Promise<void> {
-        await this._mongoose.connect(DB_URL).catch(() => {
-            console.log("Error while connecting to the BD.");
+        await this._mongoClient.connect().catch(() => {
+            this._collection = this._mongoClient.db('chat-test').collection('users');
+            // tslint:disable-next-line:no-console
+            console.log(this._collection);
         });
     }
 
     public isConnected(): boolean {
-        return this._mongoose.connection.readyState === 1;
+        return this._mongoClient.isConnected();
+    }
+
+    public async disconnectFromBD(): Promise<void> {
+        await this._mongoClient.close();
     }
 }
